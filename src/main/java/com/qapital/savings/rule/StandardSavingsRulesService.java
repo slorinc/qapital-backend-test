f470d3231641dcb01338e2c73c2701a215feb746
slorinc <slorinc@gmail.com>
2017-05-21 17:18:19 +0200

    Implement rule execution method

--- src/main/java/com/qapital/savings/rule/StandardSavingsRulesService.java
+++ src/main/java/com/qapital/savings/rule/StandardSavingsRulesService.java
@@ -1,12 +1,17 @@
 package com.qapital.savings.rule;
 
+import com.qapital.bankdata.transaction.Transaction;
 import com.qapital.bankdata.transaction.TransactionsService;
 import com.qapital.savings.event.SavingsEvent;
+import com.qapital.savings.event.SavingsEvent.EventName;
+import org.joda.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class StandardSavingsRulesService implements SavingsRulesService {
@@ -36,7 +41,76 @@ public class StandardSavingsRulesService implements SavingsRulesService {
 
     @Override
     public List<SavingsEvent> executeRule(SavingsRule savingsRule) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+
+        if(!savingsRule.getStatus().equals(SavingsRule.Status.active)) {
+            // TODO : no specification on how to handle this, returning empty result set
+            return Collections.emptyList();
+        }
+        List<SavingsEvent> results = new ArrayList<>();
+        List<Transaction> transactions = transactionsService.latestTransactionsForUser(savingsRule.getUserId());
+        // TODO check for NPE or wrap in Optional
+        for (Transaction transaction : transactions) {
+
+            // execute only on expense transactions
+            if (transaction.getAmount() > 0) {
+                continue;
+            }
+
+            if (savingsRule.getRuleType().equals(SavingsRule.RuleType.roundup)) {
+
+                results.addAll(getSavingsEventsIfRoundingRule(savingsRule, transaction));
+
+            } else if (savingsRule.getRuleType().equals(SavingsRule.RuleType.guiltypleasure)) {
+                results.addAll(getSavingsEventsIfGuiltyPleasure(savingsRule, transaction));
+            } else {
+                throw new UnsupportedOperationException("Unsupported Rule type");
+            }
+
+        }
+
+        return results;
+
+    }
+
+    private List<SavingsEvent> getSavingsEventsIfGuiltyPleasure(SavingsRule savingsRule, Transaction transaction) {
+        if (savingsRule.getPlaceDescription().equals(transaction.getDescription())) {
+            Double savingAmount = savingsRule.getAmount();
+            List<SavingsEvent> transactionResults = savingsRule.getSavingsGoalIds().stream()
+                    .map(sg ->
+                            new SavingsEvent(savingsRule.getUserId(),
+                                    sg,
+                                    savingsRule.getId(),
+                                    EventName.rule_application,
+                                    LocalDate.now(),
+                                    savingAmount / savingsRule.getSavingsGoalIds().size(),
+                                    null,
+                                    savingsRule)).collect(Collectors.toList());
+            return transactionResults;
+        }
+        return Collections.emptyList();
+    }
+
+    private List<SavingsEvent> getSavingsEventsIfRoundingRule(SavingsRule savingsRule, Transaction transaction) {
+        Double savingAmount;
+        Double roundUpAmount = savingsRule.getAmount();
+        Double amount = -transaction.getAmount();
+
+        while (roundUpAmount - amount < 0) {
+            roundUpAmount += savingsRule.getAmount();
+        }
+
+        savingAmount = roundUpAmount - amount;
+
+        return savingsRule.getSavingsGoalIds().stream()
+                .map(sg ->
+                        new SavingsEvent(savingsRule.getUserId(),
+                                sg,
+                                savingsRule.getId(),
+                                EventName.rule_application,
+                                LocalDate.now(),
+                                savingAmount / savingsRule.getSavingsGoalIds().size(),
+                                null,
+                                savingsRule)).collect(Collectors.toList());
     }
 
 }
